b57b9ee201ec3d42eec4c8472b1e29338b485138
codergo7 <codergo7@gmail.com>
2020-08-07 16:17:11 +0200

    added SetterInjectionApp class

--- demo/src/main/java/com/example/demo/SetterInjectionApp.java
+++ demo/src/main/java/com/example/demo/SetterInjectionApp.java
@@ -1,21 +1,24 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
-public class DemoApplication {
+public class SetterInjectionApp {
 
 	public static void main(String[] args) {
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		Coach myCoach = context.getBean("myCoach", Coach.class);
 		
+		FootballCoach myCoach = context.getBean("theCoach", FootballCoach.class);
+
 		System.out.println(myCoach.getDailyWorkout());
 		System.out.println(myCoach.getDailyFortune());
+		System.out.println(myCoach.getEmail());
+		System.out.println(myCoach.getTeam());
+		
 		
 		context.close();
+
 	}
 
 }
