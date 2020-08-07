81cb339ac5e12ceeea7765ffb66189c0a934cb63
codergo7 <codergo7@gmail.com>
2020-08-07 15:20:58 +0200

    added setter injection

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,10 +10,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		Coach myCoach = context.getBean("theCoach", Coach.class);
+		//Coach myCoach = context.getBean("myCoach", Coach.class);
+		FootballCoach myCoach = context.getBean("theCoach", FootballCoach.class);
 
 		System.out.println(myCoach.getDailyWorkout());
 		System.out.println(myCoach.getDailyFortune());
+		System.out.println(myCoach.getEmail());
+		System.out.println(myCoach.getTeam());
 		
 		
 		context.close();
--- demo/src/main/java/com/example/demo/FootballCoach.java
+++ demo/src/main/java/com/example/demo/FootballCoach.java
@@ -3,6 +3,10 @@ package com.example.demo;
 public class FootballCoach implements Coach {
 	private FortuneService fortune;
 	
+	private String email;
+	private String team;
+	
+	
 	public FootballCoach() {
 		// TODO Auto-generated constructor stub
 	}
@@ -22,5 +26,24 @@ public class FootballCoach implements Coach {
 		// TODO Auto-generated method stub
 		return fortune.getDailyFortune();
 	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	
 
 }
