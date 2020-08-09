8bcde03b80bb68146a59b42af87325d2cc757c75
codergo7 <codergo7@gmail.com>
2020-08-09 14:46:59 +0200

    added init and destroy methods using @PostConstruct and @PreDestroy annotations

--- spring-demo-anotations/src/main/java/com/example/demo/TennisCoach.java
+++ spring-demo-anotations/src/main/java/com/example/demo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")  //@Scope("singleton")
+@Scope("singleton") //@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	// define filed injection. >> no need setter method
@@ -30,6 +33,7 @@ public class TennisCoach implements Coach {
 	public String getDailyWorkout() {
 		return "keep your body strong!";
 	}
+	
 
 	/*
 	 * // define a setter method for setter injection
@@ -47,5 +51,19 @@ public class TennisCoach implements Coach {
 		// TODO Auto-generated method stub
 		return fortuneService.getDailyFortune();
 	}
+	
+	// defien init method
+	
+		@PostConstruct
+		public void testPostConstructMedhod() {
+			System.out.println(">>> Used @PostConstruct annotation");
+		}
+		
+		// define destroy method
+		
+		@PreDestroy
+		public void testPreDesyroyMedhod() {
+			System.out.println( "Used @PreDestroy annotation >>>");
+		}
 
 }
