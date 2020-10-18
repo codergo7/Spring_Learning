6d8efde2eac8ae181ca024d51f08fec1b456416f
codergo7 <codergo7@gmail.com>
2020-10-18 11:15:02 +0200

    added configure(HttpSecurity http) method to ToDoSecurityConfig class and ToDoWebConfig

--- ProSpringBoot2_Todo_Exercises/todo-simple-security/src/main/java/com/aga/todosimplesecurity/config/ToDoSecurityConfig.java
+++ ProSpringBoot2_Todo_Exercises/todo-simple-security/src/main/java/com/aga/todosimplesecurity/config/ToDoSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.aga.todosimplesecurity.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 /*
@@ -31,10 +33,49 @@ password.
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder())
-                .withUser("apress")
+                .withUser("aga")
                 .password(passwordEncoder().encode("springboot2"))
                 .roles("ADMIN", "USER");
     }
+
+
+    /*
+    The new modification
+show how HttpSecurity is being configured. First, its adding requestMatchers, which
+point to common locations, such as the static resources (static/* ). This is where
+CSS, JS, or any other simple HTML can live and doesn’t need any security. Then it uses
+anyRequest, which should be fullyAuthenticated. this means that the /api/* will be.
+Then, it uses formLogin to specify with loginPage("/login") that it is the endpoint
+for finding the login page. Next, declare the logout and its endpoint ("/logout"); if the
+logout is successful, it redirects to the "/login" endpoint/page.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(
+                        PathRequest
+                                .toStaticResources()
+                                .atCommonLocations()).permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login");
+    }
+
+
+    /*
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .httpBasic();
+    }
+
+ */
 
     /*
     BCryptPasswordEncoder. In this code you are using the
